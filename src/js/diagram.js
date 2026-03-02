838596acdf2a14c67f2b5bcf6ff868fed8b8b6cd
Victoria Helin <vickanhelin@hotmail.com>
2026-03-02 17:13:58 +0100

    skapat arrayer som lagrar antal ansökningar och lagt till de högsta som data i Chart 1

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -2,22 +2,33 @@
 
 const url = `https://mallarmiun.github.io/Frontend-baserad-webbutveckling/Moment%205%20-%20Dynamiska%20webbplatser/statistik_sokande_ht25.json`;
 
+let allCourses = [];
+
+let sortedCourses = [];
+
 document.addEventListener("DOMContentLoaded", async () => {
     loadCourses();
+
+
 });
 
 async function loadCourses() {
-    
+
 
     try {
         const response = await fetch(url);
         const courses = await response.json();
         // lagrar alla kurser i global array
-       let allCourses = courses;
-       allCourses.forEach(course => {
-        console.log(course.name, course.applicantsTotal);
-       });
-        
+        allCourses = courses;
+        allCourses.forEach(course => {
+            sortedCourses.push(course.applicantsTotal);
+            sortedCourses.sort((a, b) => a - b);
+
+        });
+        let reversedAplNumb = sortedCourses.reverse();
+        let highestAplNumb = [reversedAplNumb[0], reversedAplNumb[1], reversedAplNumb[2], reversedAplNumb[3], reversedAplNumb[4], reversedAplNumb[5]];
+        console.log(highestAplNumb);
+        addChartData(highestAplNumb);
 
     }
     catch (error) {
@@ -25,38 +36,40 @@ async function loadCourses() {
     }
 }
 
-/* för Chart.js */
+function addChartData(highestAplNumb) {
+    /* för Chart.js */
 
-const chartDiv = document.getElementById("myChart");
+    const chartDiv = document.getElementById("myChart");
 
-new Chart(chartDiv, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
-    }, 
-    options: {
-        backgroundColor: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
-    }
-});
+    new Chart(chartDiv, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [highestAplNumb[0], highestAplNumb[1], highestAplNumb[2], highestAplNumb[3], highestAplNumb[4], highestAplNumb[5]],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            backgroundColor: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
+        }
+    });
 
-const chartDiv2 = document.getElementById("myChart2");
+    const chartDiv2 = document.getElementById("myChart2");
 
-new Chart(chartDiv2, {
-    type: 'pie',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
-    }, 
-    options: {
-        backgroundColor: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
-    }
-});
+    new Chart(chartDiv2, {
+        type: 'pie',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            backgroundColor: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
+        }
+    });
+}
